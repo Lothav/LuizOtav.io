09c4abb052a85522afeeac0ba9fd5e94e12be988
Luiz0tavio <luizotrv@gmail.com>
2016-11-15 23:44:28 -0200

    changes in Luiz0tavio/coco_vs_paint

---
+++ cp_coco_vs_paint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ../coco_vs_paint/index.html ./coco_vs_paint &&
+cp ../coco_vs_paint/coco_vs_paint.js ./coco_vs_paint &&
+cp -r ../coco_vs_paint/assets ./coco_vs_paint &&
+git add * && git commit -m "changes in Luiz0tavio/coco_vs_paint" && git push origin master
