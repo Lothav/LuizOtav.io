06bc00b209884bf212e858db392e66fce908ac7b
Luiz Otavio <luizotavioresende@hotmail.com>
2016-11-16 11:08:00 -0200

    Delete cp_coco_vs_paint.sh

--- cp_coco_vs_paint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp ../coco_vs_paint/coco_vs_paint.js ./coco_vs_paint &&
-cp -r ../coco_vs_paint/assets ./coco_vs_paint &&
-git add * && git commit -m "changes in Luiz0tavio/coco_vs_paint" && git push origin master
