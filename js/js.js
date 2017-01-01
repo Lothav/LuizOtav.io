065913b234c575976c402fa6e9858b6305b5f2c9
Luiz Otavio <luizotrv@gmail.com>
2017-01-01 02:13:39 -0200

    React prod -> first commit react

--- js/js.js
+++ js/js.js
@@ -13,10 +13,10 @@ $(window).load(function(){
     },800);
 
     $('svg.paint_god path').each(function(){
-        var comprimento = $(this).get(0).getTotalLength();
-        var comprimentoArredondado = Math.round(comprimento);
-        $(this).attr('stroke-dasharray', comprimentoArredondado);
-        $(this).attr('stroke-dashoffset', comprimentoArredondado);
+        var len = $(this).get(0).getTotalLength();
+        var len_round = Math.round(len);
+        $(this).attr('stroke-dasharray', len_round);
+        $(this).attr('stroke-dashoffset', len_round);
     });
 
 
